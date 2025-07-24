11d114b4d6f9a73bfbf979d5e3de5cedb329d1fc
sontyubo <sontyubo@gmail.com>
2025-07-24 15:54:05 +0900

    add: custom

---
+++ custom/custom_scheduler/__init__.py
@@ -0,0 +1,3 @@
+from .linear_warmup_cosine import linear_warmup_cosine_scheduler
+
+__all__ = ["linear_warmup_cosine_scheduler"]
---
+++ custom/custom_scheduler/linear_warmup_cosine.py
@@ -0,0 +1,11 @@
+import math
+from torch.optim.lr_scheduler import LambdaLR
+
+def linear_warmup_cosine_scheduler(optimizer, warmup_steps, total_steps):
+    def lr_lambda(current_step):
+        if current_step < warmup_steps:
+            return float(current_step) / float(max(1, warmup_steps))
+        progress = float(current_step - warmup_steps) / float(max(1, total_steps - warmup_steps))
+        return 0.5 * (1.0 + math.cos(math.pi * progress))
+    
+    return LambdaLR(optimizer, lr_lambda)
